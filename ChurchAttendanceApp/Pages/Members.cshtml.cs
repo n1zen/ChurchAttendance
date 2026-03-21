fe5b4644d059fb8d90a098dbf4752efefc503467
n1zen <thisisnez1.2021@gmail.com>
2026-03-21 19:03:01 +0800

    create table for members list

--- ChurchAttendanceApp/Pages/Members.cshtml.cs
+++ ChurchAttendanceApp/Pages/Members.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ChurchAttendanceApp.Services;
+using ChurchAttendanceApp.Models;
 
 namespace ChurchAttendanceApp.Pages
 {
     public class MembersModel : PageModel
     {
+        private readonly MemberService _memberService;
+
+        public List<Member> Members { get; set; } = [];
+
+        public MembersModel(MemberService memberService)
+        {
+            _memberService = memberService;
+        }
         public void OnGet()
         {
+            Members = _memberService.GetAll();
         }
     }
 }
