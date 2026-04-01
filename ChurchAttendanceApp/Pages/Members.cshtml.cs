dfcfbc561a80890b94154132e32503badc1cfa6a
n1zen <thisisnez1.2021@gmail.com>
2026-04-01 13:34:43 +0800

    Add CSV export for members list on Members page
    Admins can now export the list of members as a CSV file via a new "Export as CSV" button. This includes UI updates and backend logic to generate and return the CSV using an injected ExportService.

--- ChurchAttendanceApp/Pages/Members.cshtml.cs
+++ ChurchAttendanceApp/Pages/Members.cshtml.cs
@@ -10,19 +10,30 @@ namespace ChurchAttendanceApp.Pages
     public class MembersModel : PageModel
     {
         private readonly MemberService _memberService;
+        private readonly ExportService _exportService;
         public List<Member> Members { get; set; } = [];
 
         public DateOnly dateToday = DateOnly.FromDateTime(DateTime.Today);
 
-        public MembersModel(MemberService memberService)
+        public MembersModel(MemberService memberService, ExportService exportService)
         {
             _memberService = memberService;
+            _exportService = exportService;
         }
         public void OnGet()
         {
             Members = _memberService.GetAll();
         }
 
+        public async Task<IActionResult> OnPostExport()
+        {
+            var members = _memberService.GetAll();
+
+            var columns = new List<string> { "Id", "MemberId", "Name", "Gender", "Birthday", "DateBaptized", "ChurchOfOrigin", "Address", "Email", "Phone", "MembershipStatus", "AttendanceDate", "DateRegistered" };
+
+            var file =  await _exportService.ExportMembers(members, columns);
+            return File(file, "text/csv", $"members.csv");
+        }
         public bool CheckAttendance(Member member)
         {
             if (member.AttendanceDate == dateToday)
